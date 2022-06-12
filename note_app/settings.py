3230eaf91784ab18ac4d79782e9565774f029bcb
fabulouslevi <iamosayame@gmail.com>
2022-06-12 16:02:57 -0600

    postgres

--- note_app/settings.py
+++ note_app/settings.py
@@ -82,12 +82,12 @@ WSGI_APPLICATION = 'note_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-#DATABASES = {
- #   'default': {
-#      'ENGINE': 'django.db.backends.sqlite3',
-#       'NAME': BASE_DIR / 'db.sqlite3',
- #   }
-#}
+DATABASES = {
+    'default': {
+      'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
